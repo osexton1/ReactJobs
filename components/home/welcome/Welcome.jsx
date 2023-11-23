fe5afe636a9618d3b08d221136bfa7ac5f610838
osexton1 <odhran.sexton@gmail.com>
2023-11-23 12:01:03 +0000

    UI/UX of Home page finished. Dummy data used instead of querying API

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -55,9 +55,12 @@ const Welcome = () => {
                 router.push(`/search/${item}`)
               }}
             >
-              <Text>{item}</Text>
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
     </View>
